6fadf569fc04d4565fe4b3883430423e66db9e78
lucas10077 <lucaslima10077@gmail.com>
2019-10-16 19:15:30 -0300

    criando arquivo

--- EstudoInputoOutput/1_LidandoComStreamDiretamente.cs
+++ EstudoInputoOutput/1_LidandoComStreamDiretamente.cs
@@ -1,46 +1,46 @@
-﻿//using EstudoInputOutput.Models;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using EstudoInputOutput.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//using System.IO; // IO = Input e Output
+using System.IO; // IO = Input e Output
 
-//namespace EstudoInputOutput
-////{
-//////    partial class Program
-//////    {
-//////        static void LidandoComFileStreamDiretamente()
-//////        {
-//////            var enderecoDoArquivo = "contas.txt";
+namespace EstudoInputOutput
+{
+    partial class Program
+    {
+        static void LidandoComFileStreamDiretamente()
+        {
+            var enderecoDoArquivo = "contas.txt";
 
-//////            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-//////            {
-//////                var buffer = new byte[1024]; // Criando uma array temporaria
-//////                var numeroDeBytesLidos = -1;
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            {
+                var buffer = new byte[1024]; // Criando uma array temporaria
+                var numeroDeBytesLidos = -1;
 
-//////                while (numeroDeBytesLidos != 0)
-//////                {
-//////                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, buffer.Length);
-//////                    Console.WriteLine($"Bytes lidos: {numeroDeBytesLidos}");
-//////                    EscreverBuffer(buffer, numeroDeBytesLidos);
-//////                }
-//////            }
-//////        }
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, buffer.Length);
+                    Console.WriteLine($"Bytes lidos: {numeroDeBytesLidos}");
+                    EscreverBuffer(buffer, numeroDeBytesLidos);
+                }
+            }
+        }
 
-//////        static void EscreverBuffer(byte[] buffer, int bytesLidos)
-//////        {
-//////            var utf8 = Encoding.Default; 
+        static void EscreverBuffer(byte[] buffer, int bytesLidos)
+        {
+            var utf8 = Encoding.Default;
 
-//////            var texto = utf8.GetString(buffer, 0, bytesLidos);
-//////            Console.Write(texto);
+            var texto = utf8.GetString(buffer, 0, bytesLidos);
+            Console.Write(texto);
 
-//////            //foreach (var meuByte in buffer)
-//////            //{
-//////            //    Console.Write(meuByte);
-//////            //    Console.Write(" ");
-//////            //}
-//////        }
-//////    }
-//////}
+            //foreach (var meuByte in buffer)
+            //{
+            //    Console.Write(meuByte);
+            //    Console.Write(" ");
+            //}
+        }
+    }
+}
---
+++ EstudoInputoOutput/3_CriandoArquivo.cs
@@ -0,0 +1,30 @@
+﻿using EstudoInputOutput.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO; // IO = Input e Output
+
+namespace EstudoInputOutput
+{
+    partial class Program
+    {
+        static void CriarArquivo()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo,FileMode.Create))
+            {
+                var contaComoString = "252,52582,2546.5,lucas lima";
+                var enconding = Encoding.UTF8;
+
+                var bytes = enconding.GetBytes(contaComoString);
+
+                fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+            }
+        }
+
+    }
+}
